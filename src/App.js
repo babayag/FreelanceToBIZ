0ebf3f04ac961efa69ad3aaed01f39e91d69770d
babayag <ironclock6@gmail.com>
2019-03-31 18:13:41 +0200

    change the home page website

--- src/App.js
+++ src/App.js
@@ -11,10 +11,10 @@ class App extends Component {
     return (
       <div>
         <BarNav/>
-        <BrowserRouter basename="FreelanceToBusiness/">
+        <BrowserRouter>
         <Switch>
-          <Route   exact path="/" component={Home} />
-          <Route   path="/about" component={About} />
+          <Route  exact path="/" component={Home} />
+          <Route  path="/about" component={About} />
         </Switch>
       </BrowserRouter>
       </div>
--- src/components/barNav.js
+++ src/components/barNav.js
@@ -6,7 +6,7 @@ export class BarNav extends Component
     render(){
         return(
         <Navbar bg="dark" variant="dark" expand="lg">
-            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+            <Navbar.Brand href="#home">FreelanceToBIZ</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto">
