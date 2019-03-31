041befad77b6f76f0aa88e108bd8e5b550069a35
babayag <ironclock6@gmail.com>
2019-03-31 17:37:10 +0200

    Updates

---
+++ precache-manifest.0f17e1643b7bd692182a3132ac0f14be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e73956e9971fa4b3e91f",
+    "url": "/FreelanceToBIZ/static/js/runtime~main.fb72b777.js"
+  },
+  {
+    "revision": "aa29a94456418f1c247b",
+    "url": "/FreelanceToBIZ/static/js/main.78d11620.chunk.js"
+  },
+  {
+    "revision": "b0dfc50df27992258563",
+    "url": "/FreelanceToBIZ/static/js/2.4a0df4ad.chunk.js"
+  },
+  {
+    "revision": "aa29a94456418f1c247b",
+    "url": "/FreelanceToBIZ/static/css/main.0401eb98.chunk.css"
+  },
+  {
+    "revision": "8b5057dfa130fbd1d48b6ba9e1ae8e46",
+    "url": "/FreelanceToBIZ/index.html"
+  }
+];
